053b2c7f2b6306c35820571147f2a4fa53db1206
Lunirs <dhong0925@gmail.com>
2022-05-19 12:08:09 -0400

    Added addproject function and pseudocode for function

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,20 @@
 // DEPENDENCIES-----------------
-    // Display time element
-    // Display modal 
-    // table row element
+// Display time element
+// Display modal
+// table row element
 
 // STATE------------------------
 
 // HELPER FUNCTIONS-------------
-    // Run the clock
+// Run the clock
+// add table element when user clicks add button
+
+var addProject = function () {
+  //append new elements to <td> tags
+  // add delete project button
+  // add class to modal to hide it
+  //append the new row element to the project display element
+};
 // USER INTERACTIONS------------
 
 // INITIALIZATION---------------
-
